c4dce27064459f5ce7478026786058fc3bdd99ab
dollabillgates <31433587+dollabillgates@users.noreply.github.com>
2023-10-09 11:31:01 -0400

    Update utils.py to batch node operations to train cue memory for computation

--- graphium/nn/pyg_layers/utils.py
+++ graphium/nn/pyg_layers/utils.py
@@ -59,30 +59,26 @@ class PreprocessPositions(nn.Module):
         # so that it can be added to the original node features
         self.node_proj = nn.Linear(self.num_kernel, self.embed_dim)
 
+    # Batching Function
+    def compute_delta_pos_in_batches(pos, batch_size):
+        batch, n_node, _ = pos.shape
+        delta_pos = torch.zeros((batch, n_node, n_node, 3), device=pos.device, dtype=pos.dtype)
+    
+        for i in range(0, n_node, batch_size):
+            for j in range(0, n_node, batch_size):
+                slice_i = slice(i, i+batch_size)
+                slice_j = slice(j, j+batch_size)
+                delta_pos[:, slice_i, slice_j] = pos[:, slice_i, :].unsqueeze(2) - pos[:, slice_j, :].unsqueeze(1)
+    
+        return delta_pos
+    
     def forward(
         self, batch: Batch, max_num_nodes_per_graph: int, on_ipu: bool, positions_3d_key: str
     ) -> Tuple[Tensor, Tensor]:
-        r"""
-        Inputs:
-            batch:
-                Batch object.
-            max_num_nodes_per_graph:
-                Maximum number of nodes per graph.
-            on_ipu:
-                If model rus on IPU.
-            positions_3d_key:
-                The key of the pyg graph object that contains the 3D positions.
-
-        """
-
         pos = batch[positions_3d_key]
         if self.first_normalization is not None:
             pos = self.first_normalization(pos)
         batch_size = None if pos.device.type != "ipu" else batch.graph_is_true.shape[0]
-        # batch_size = None if batch.feat.device.type != "ipu" else batch.graph_is_true.shape[0] #[Andy] batch.feat is only available after passing through layers, not a good attribute to check
-        # pos: [batch, nodes, 3]
-        # padding_mask: [batch, nodes]
-        # idx: [totoal_nodes]
         pos, mask, idx = to_dense_batch(
             pos,
             batch=batch.batch,
@@ -90,68 +86,30 @@ class PreprocessPositions(nn.Module):
             max_num_nodes_per_graph=max_num_nodes_per_graph,
             drop_nodes_last_graph=on_ipu,
         )
-        # check nan with the pos from to_dense_batch,
-        # and generate mask. 1 for nan, 0 for other values.
-        # pos consists of real nodes and padding nodes
-        # for real nodes, if 3d position does not exit, it is nans. For padding nodes, 3d positions will be 0
-        # if the first node of a molecule has 3d position as nan, the whole molecule will be masked out.
-        # [batch]
         nan_mask = torch.isnan(pos)[:, 0, 0]
-        # apply nan_mask on pos so that it does not give nan gradient
-        # when applying gaussian kernels
         pos.masked_fill_(nan_mask.unsqueeze(1).unsqueeze(2), 0.0)
-        # we need the opposite of mask output
         padding_mask = ~mask
-        # [batch, nodes]
         batch, n_node, _ = pos.shape
-        # [batch, nodes, nodes, 3]
-        delta_pos = pos.unsqueeze(1) - pos.unsqueeze(2)
-        # [batch, nodes, nodes]
+        batch_size = 1024  # Or any other batch size that fits in your memory
+        delta_pos = compute_delta_pos_in_batches(pos, batch_size) # Implement Batching function
         distance = delta_pos.norm(dim=-1).view(-1, n_node, n_node)
-        # [batch, nodes, nodes, num_kernel]
         distance_feature = self.gaussian(distance)
-        # [batch, nodes, nodes, num_heads]
         attn_bias = self.gaussian_proj(distance_feature)
-        # [batch, num_heads, nodes, nodes]
         attn_bias = attn_bias.permute(0, 3, 1, 2).contiguous()
-        # apply padding_mask on attn_bias
-        # unsqueezed mask size: [batch, 1, 1, nodes] apply on tensor [batch, num_heads, nodes, nodes]
-        attn_bias.masked_fill_(
-            padding_mask.unsqueeze(1).unsqueeze(2),
-            float("-1000"),
-        )
-        # apply nan_mask on attn_bias
-        # unsqueezed mask size: [batch, 1, 1, 1] apply on tensor [batch, num_heads, nodes, nodes]
-        attn_bias.masked_fill_(
-            nan_mask.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1),
-            0.0,
-        )
-        # apply padding_mask on distance_feature
-        # unsqueezed mask size: [batch, 1, nodes, 1] apply on tensor [batch, nodes, nodes, num_kernel]
+        attn_bias.masked_fill_(padding_mask.unsqueeze(1).unsqueeze(2), float("-1000"))
+        attn_bias.masked_fill_(nan_mask.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1), 0.0)
         distance_feature.masked_fill_(padding_mask.unsqueeze(1).unsqueeze(-1).to(torch.bool), 0.0)
-        # [batch, nodes, num_kernel]
         distance_feature_sum = distance_feature.sum(dim=-2)
-        # Output of GaussianLayer is FP32, cast to dtype of self.node_proj here
         distance_feature_sum = distance_feature_sum.to(self.node_proj.weight.dtype)
-        # [batch, nodes, embed_dim]
         node_feature = self.node_proj(distance_feature_sum)
-        # apply nan_mask on node_feature
-        # unsqueezed mask size: [batch, 1, 1] apply on tensor [batch, nodes, embed_dim]
         node_feature.masked_fill_(nan_mask.unsqueeze(1).unsqueeze(2).to(torch.bool), 0.0)
-        # [total_nodes, embed_dim]
         node_feature = to_sparse_batch(node_feature, idx)
 
         return attn_bias, node_feature
 
-
 class GaussianLayer(nn.Module):
+    class GaussianLayer_batch(nn.Module):
     def __init__(self, num_kernels=128, in_dim=3):
-        r"""
-            Gaussian kernel function that applied on the all-to-all 3D distances.
-        Parameters:
-            num_kernels:
-                Number of gaussian kernel used.
-        """
         super().__init__()
         self.num_kernels = num_kernels
         self.means = nn.Embedding(1, num_kernels)
@@ -159,20 +117,22 @@ class GaussianLayer(nn.Module):
         nn.init.uniform_(self.means.weight, 0, in_dim)
         nn.init.uniform_(self.stds.weight, 0, in_dim)
 
-    def forward(self, input: Tensor) -> Tensor:
-        # [batch, nodes, nodes, 1]
+    def forward(self, input: Tensor, batch_size=1024) -> Tensor:
         input = input.unsqueeze(-1)
-        # [batch, nodes, nodes, num_kernels]
         expanded_input = input.expand(-1, -1, -1, self.num_kernels)
-        # [num_kernels]
         mean = self.means.weight.float().view(-1)
-        # [num_kernels]
-        std = self.stds.weight.float().view(-1).abs() + 0.01  # epsilon is 0.01 that matches gps++ value
+        std = self.stds.weight.float().view(-1).abs() + 0.01 
         pre_exp_factor = (2 * math.pi) ** 0.5
-        # [batch, nodes, nodes, num_kernels]
-        tensor_with_kernel = torch.exp(-0.5 * (((expanded_input - mean) / std) ** 2)) / (pre_exp_factor * std)
-        return tensor_with_kernel
 
+        tensor_with_kernel = torch.zeros_like(expanded_input)
+
+        for i in range(0, input.shape[1], batch_size):
+            for j in range(0, input.shape[2], batch_size):
+                batch_input = expanded_input[:, i:i+batch_size, j:j+batch_size, :]
+                batch_kernel = torch.exp(-0.5 * (((batch_input - mean) / std) ** 2)) / (pre_exp_factor * std)
+                tensor_with_kernel[:, i:i+batch_size, j:j+batch_size, :] = batch_kernel
+
+        return tensor_with_kernel
 
 def triplets(
     edge_index: Tensor,
