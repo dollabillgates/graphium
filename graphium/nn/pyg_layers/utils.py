d35d04f3d7c75d4272641904845b9066552ab81a
dollabillgates <31433587+dollabillgates@users.noreply.github.com>
2023-10-14 19:26:39 -0400

    fix node batching inconsistency

--- graphium/nn/pyg_layers/utils.py
+++ graphium/nn/pyg_layers/utils.py
@@ -64,12 +64,13 @@ class PreprocessPositions(nn.Module):
                 delta_pos_batch = graph_pos[slice_i].unsqueeze(1) - graph_pos
                 distance = delta_pos_batch.norm(dim=-1)
                 distance_features = self.gaussian(distance)
-
+            
                 distance_features_sum[slice_i] += distance_features.sum(dim=-2)
                 attn_bias_per_head = self.gaussian_proj(distance_features).permute(2, 0, 1)
-                attn_bias += attn_bias_per_head
-
-                del distance_features, delta_pos_batch, distance            
+            
+                attn_bias[:, slice_i, :n_node] = attn_bias_per_head
+            
+                del distance_features, delta_pos_batch, distance 
 
             attn_bias.masked_fill_(padding_mask[graph].unsqueeze(0), float("-1000"))
             node_feature = self.node_proj(distance_features_sum)
